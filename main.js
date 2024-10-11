ebee031e254d8207146583c5e545d6e693e1e673
hassan_belaqdour <hassanbelaqdour@gmail.com>
2024-10-11 17:27:49 +0100

    js project

---
+++ main.js
@@ -0,0 +1,94 @@
+let space ="--------------------------------------------";
+     
+    //Déclaration et affectation de variables
+
+        let prenom1 ="hamza";
+        console.log(prenom1);
+
+console.log(space);
+    
+     //Manipulation de variables numériques
+
+const a=50;
+const b=30;
+
+    console.log("la somme", a+b);
+    console.log("la deffirence",a-b);
+    console.log("le produit", a*b);
+    console.log("le quotient", a/b);
+
+console.log(space);
+
+     //Concaténation de chaînes de caractères
+
+        let prenom2 ="hassan";
+        let nom1 ="belaqodur";
+
+        let nomcomplet = prenom2+" " + nom1;
+        console.log("le nom complet est",nomcomplet)
+
+console.log(space);
+
+     //Utilisation des variables booléennes
+
+        let age1 = 18;
+        let majeur = age1 <= 18;
+        console.log(majeur);
+
+console.log(space);
+
+    //Échange de valeurs entre deux variables
+
+        let x = 10;
+        let y = 20;
+        x=x+y;
+        y=x-y;
+        x=x-y;
+        console.log("x = ",x);
+        console.log("y = ",y);
+
+console.log(space);
+
+    //Calcul de la circonférence d'un cercle
+
+        let rayon = 7;
+        let circonférence = 2 * 3.14 * rayon;
+        console.log("la circonférence est :",circonférence);
+
+console.log(space);
+
+    //Conversion de température
+
+        let celsius = 24;
+        let Fahrenheit = celsius * 9/5 + 32;
+        console.log("la teperature en Fahrenheit est :",Fahrenheit);
+
+console.log(space);
+
+    //Calcul de l'âge
+
+        let anneeNaissance = 2006;
+        let anneeActuelle = 2024;
+        let age2 = anneeActuelle - anneeNaissance ;
+        console.log("l'age est :", age2);
+
+console.log(space);
+
+    //Calcul du périmètre d'un rectangle
+
+        let longeur1 = 14;
+        let longeur2 = 20;
+        let P = 2 * (longeur1 + longeur2);
+        console.log("périmètre d'un rectangle est :",P);
+
+console.log(space);
+
+    //Moyenne de trois nombres
+
+        let note1 = 13;
+        let note2 = 16;
+        let note3 = 8;
+        let moyenne = (note1+note2+note3)/3;
+        console.log("la moyenne generale est :",moyenne);
+
+
